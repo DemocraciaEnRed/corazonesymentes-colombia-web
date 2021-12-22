75471ce8a7167b81ee5599533a868780b9f87ee9
DevHormiga <devhormiga@gmail.com>
2021-12-22 13:37:18 -0300

    pre-view link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -31,17 +31,17 @@ import Nocomocuento from '../components/nocomocuento.svelte';
 
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://metatags.io/">
+	<meta property="og:url" content="https://corazonesymentes.co/">
 	<meta property="og:title" content="Corazones y Mentes - Colombia">
 	<meta property="og:description" content="EL INTERNET ES ÚTIL PARA TODOS, PERO QUEREMOS QUE APRENDAS A USARLO PARA QUE SAQUES LO MEJOR DE ÉL Y NO LO PEOR DE TI.">
-	<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+	<meta property="og:image" content="logo-horizontal-peque.png">
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image">
-	<meta property="twitter:url" content="https://metatags.io/">
+	<meta property="twitter:url" content="https://corazonesymentes.co/">
 	<meta property="twitter:title" content="Corazones y Mentes - Colombia">
 	<meta property="twitter:description" content="EL INTERNET ES ÚTIL PARA TODOS, PERO QUEREMOS QUE APRENDAS A USARLO PARA QUE SAQUES LO MEJOR DE ÉL Y NO LO PEOR DE TI.">
-	<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<meta property="twitter:image" content="logo-horizonta-twetterl.png">	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-215403556-1"></script>
 </svelte:head>
