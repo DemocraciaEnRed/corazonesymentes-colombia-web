2768a01a990ac662b98d6112d31199b31aed39e6
DevHormiga <devhormiga@gmail.com>
2021-12-22 12:26:53 -0300

    metaTags share preview

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,9 +23,25 @@ import Nocomocuento from '../components/nocomocuento.svelte';
 
 
 </script>
-<svelte:head>
-	<title>Corazones Y Mentes</title>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>Corazones y Mentes - Colombia</title>
+	<meta name="title" content="Corazones y Mentes - Colombia">
+	<meta name="description" content="EL INTERNET ES ÚTIL PARA TODOS, PERO QUEREMOS QUE APRENDAS A USARLO PARA QUE SAQUES LO MEJOR DE ÉL Y NO LO PEOR DE TI.">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://metatags.io/">
+	<meta property="og:title" content="Corazones y Mentes - Colombia">
+	<meta property="og:description" content="EL INTERNET ES ÚTIL PARA TODOS, PERO QUEREMOS QUE APRENDAS A USARLO PARA QUE SAQUES LO MEJOR DE ÉL Y NO LO PEOR DE TI.">
+	<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://metatags.io/">
+	<meta property="twitter:title" content="Corazones y Mentes - Colombia">
+	<meta property="twitter:description" content="EL INTERNET ES ÚTIL PARA TODOS, PERO QUEREMOS QUE APRENDAS A USARLO PARA QUE SAQUES LO MEJOR DE ÉL Y NO LO PEOR DE TI.">
+	<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-215403556-1"></script>
 </svelte:head>
